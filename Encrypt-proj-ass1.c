cef1d42bdec507d10628e25ae1a1d027927cef84
Harry-Wheeler <c3324695@uon.edu.au>
2019-04-09 11:56:27 +1000

    Made rotation cipher encrypt/decrypt functions work letter by letter.

--- Encrypt-proj-ass1.c
+++ Encrypt-proj-ass1.c
@@ -1,33 +1,61 @@
 #include <stdio.h>
 
-void rot_encrypt(const char *message, int key); //limit of 200 character message, can be increased.
-void rot_decrypt(const char *en_message, int key); //Also limited to 200.
+char rot_encrypt(char letter, int key); //limit of 200 character message, can be increased.
 
 int main() {
-	const char message[] = "WE SHOULD ATTACK NOW"; //Temporary measure to input an example message.
-	const char en_message[] = "ZH VKRXOG DWWDFN QRZ";
+	int index = 0; //Used in each case to iterate through letters in input_string.
+	char letter; //Will become each subsequent letter of input string in each case.
+	const char input_string[] = "WE SHOULD ATTACK NOW"; //Temporary measure to input an example message.
+	//const char input_string[] = "ZH VKRXOG DWWDFN QRZ"; //Temporary measure to input example coded message.
+	int key = 3; //Temporary measure to input key for rotation cipher.
+	
 	printf("Select option (1-6)\n1 for rotation encryption.\n2 for decryption of rotation cipher given key.\n3 for substitution encryption. \n4 for decryption of substitution given key. \n5 for decryption of rotation without key. \n6 for decryption of substitution without key. \nResponse: ");
+	//Above line prompts use for a choice of the 6 operations this program performs.
 	int menu_response;
-	scanf("%d", &menu_response);
-	/*if (menu_response < 1 || menu_response > 6) { //This if() should be made unnecessary by default of switch
-		//printf("Error: enter number between 1 and 6.");
-		//At this point I would call the menu function again if it were a function.
-	}*/
-	switch(menu_response) {
+	scanf("%d", &menu_response); //User's response is recorded in variable 'menu_response'.
+	switch(menu_response) {		//switch() only runs code under case corresponding to menu_response value.
 		//const char message[] = "WE SHOULD ATTACK NOW"; //message ends up being wrong if defined here...?
 		case 1: 
-			//const char message[] = "WE SHOULD ATTACK NOW"; //Won't compile if defined here.
-			printf("For the moment the message to be encrypted is defaulted to %s, but you can pick the encryption key (-26 < key < 26): ", message);
-			int key;
-			scanf("%d", &key);
-			rot_encrypt(message, key);
+			//const char message[] = "WE SHOULD KILL CAESER";
+			printf("For the moment the message is %s, and the key is %d.\n", input_string, key);
+			char en_message[200]; //This value is the limit to message length.
+			index = 0; //index is counter to iterate through message letters in WHILE.
+			//int key = 3;
+			letter = input_string[index]; //letter now becomes the first letter in message.
+			while(letter) { //Implicitly means while (letter != 0). Because last char in any string is NULL, this will ENDWHILE at end of string.
+				en_message[index] = rot_encrypt(letter, key);
+				index++;
+				letter = input_string[index];
+			}
+			//Below just prints resultant string.
+			index = 0;
+			letter = en_message[index];
+			while (letter) {
+				printf("%c", letter);
+				index++;
+				letter = en_message[index];
+			}
 			break;
-		case 2: 
-			//const char en_message[] = "ZH VKRXOG DWWDFN QRZ"; //Also won't compile if defined here.
-			printf("For the moment the encrypted message is defaulted to %s, but you can tell me the encryption key (-26 < key < 26): ", en_message);
-			//int key; //Apparently the previous declaration of 'int key' stands in this case even though those lines didn't run.
-			scanf("%d", &key);
-			rot_decrypt(en_message, key);
+		case 2:
+			printf("For the moment the coded message is %s, and the key is %d.\n", input_string, key);
+			char message[200]; //This value is the limit to message length.
+			key = -key; //Since encryption and decryption of rotation cipher are only distinguished by whether key is added or subtracted,
+						//same function can be used for decryption once key has been made negative.
+			index = 0; //index is counter to iterate through message letters in WHILE. 
+			letter = input_string[index]; //letter now becomes the first letter in message.
+			while(letter) { //Implicitly means while (letter != 0). Because last char in any string is NULL, this will ENDWHILE at end of string.
+				message[index] = rot_encrypt(letter, key);
+				index++;
+				letter = input_string[index];
+			}
+			//Below just prints resultant string.
+			index = 0;
+			letter = message[index];
+			while (letter) {
+				printf("%c", letter);
+				index++;
+				letter = message[index];
+			}			
 			break;
 		case 3: //call appropriate function.
 		case 4: //call appropriate function.
@@ -47,43 +75,22 @@ on its own, or the function must act only on one element
 (letter) of the message array at a time. (Pointers could
 solve this but they would be the most difficult route)
 	
-I chose the former. Although my code will be
-more convoluted and less reusable overall, the functions 
-will be entirely self contained.*/
-void rot_encrypt(const char *message, int key) { //message is original message to be encrypted, key is distance letters will shift along alphabet.
-	char en_message[200]; //This value is the limit to message length.
-	int index = 0; //index is counter to iterate through message letters in WHILE.
-	char letter = message[index]; //letter now becomes the first letter in message.
-	while (letter) { //Implicitly means while (letter != 0). Because last char in any string is null, this will ENDWHILE at end of string.
-		if (letter == 32)			//This if() ensures that spaces remain unchanged.
-			en_message[index] = 32;
-		else
-			en_message[index] = ((letter - 65) + key + 2*26)%26 + 65; /*Shifts letter by key amount along alphabet, uses modulus to ensure
-		                                                         'rotation' back into alphabet, then converts back to CAPS ASCII. Also adds
-																 26 to ensure term modulus affects is positive.*/
-		index++;												
-		letter = message[index];
-	}
-	printf("Encrypted version: %s", en_message);
-}
+I initially chose the former, but changed my mind because
+in order to work with file I/O I will need to read from a 
+file, process and write to a new file one letter at a time.
 
-/*The only differences between this function and the one
-above is that the strings 'en_message' and 'message' swap 
-names, and the key is subtracted rather than added from
-ASCII value of each letter.*/
-void rot_decrypt(const char *en_message, int key) { //en_message is coded message to be decrypted, key is distance letters were shifted along alphabet.
-	char message[200]; //This value is the limit to message length.
-	int index = 0; //index is counter to iterate through message letters in WHILE.
-	char letter = en_message[index]; //letter now becomes the first letter in message.
-	while (letter) { //Implicitly means while (letter != 0). Because last char in any string is null, this will ENDWHILE at end of string.
-		if (letter == 32)			//This if() ensures that spaces remain unchanged.
-			message[index] = 32;
-		else
-			message[index] = ((letter - 65) - key + 2*26)%26 + 65; /*Shifts letter by key amount along alphabet, uses modulus to ensure
-		                                                         'rotation' back into alphabet, then converts back to CAPS ASCII. Also adds
-																 26 to ensure term modulus affects is positive.*/
-		index++;										
-		letter = en_message[index];
+This function is used to both encrypt and decrypt rotation
+ciphers, since the two operations are so similar. For 
+decryption, key is made negative before the function is run.*/
+char rot_encrypt(char letter, int key) { //'letter' is letter to be encrypted, 'key' is distance letters will shift along alphabet.
+	
+	if (letter == 32)			//This if() ensures that spaces remain unchanged.
+		return 32;
+	else {
+		char en_letter = ((letter - 65) + key + 2*26)%26 + 65; 
+		//Above line shifts letter by key amount along alphabet, uses modulus to ensure
+	    //'rotation' back into alphabet, then converts back to CAPS ASCII. Also adds
+		//26 to ensure term modulus affects is positive.
+		return en_letter;
 	}
-	printf("Decrypted version: %s", message);
 }
